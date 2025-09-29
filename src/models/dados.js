70878c2b18332e7b86f0c531c407089a0f62f864
Nicolly <saantos.nick@gmail.com>
2025-09-29 09:04:25 -0300

    feat: cria dados dos bruxos

---
+++ src/models/dados.js
@@ -0,0 +1,34 @@
+const bruxos = [
+    {
+        id: 1,
+        nome: "Harry Potter",
+        idade:17,
+        casa: "Grifinória",
+    },
+    {
+        id: 2,
+        nome: "Hermione Granger",
+        idade: 17,
+        casa: "Grifinória",
+    },
+    {
+        id: 3,
+        nome: "Ron Weasley",
+        idade: 17,
+        casa: "Grifinória",
+    },
+    {
+        id: 4,
+        nome: "Draco Malfoy",
+        idade: 17,
+        casa: "Sonserina",
+    },
+    {
+        id: 5,
+        nome: "Luna Lovegood",
+        idade: 16,
+        casa: "Corvinal",
+    },
+];
+
+export default {bruxos}
